22f5ef8f1e3f8845aee4de364de9f2d5fa425934
What-ever-this-is <yuchestart@gmail.com>
2024-02-10 15:52:34 -0600

    Added a bunch of files ig

--- src/facerec.py
+++ src/facerec.py
@@ -7,9 +7,26 @@ class FaceRecognition():
         "neutral":[],
         "friendly":[]
     }
-    def __init__(self):
-        pass
+    camera:cv2.VideoCapture = None
+    camera_ID:int = None
+    def __init__(self,camera):
+        self.camera = cv2.VideoCapture(camera)
     def detect_faces(self,image_bgr,scale=0.25):
         image_rgb = cv2.cvtColor(image_bgr,cv2.COLOR_BGR2RGB)
         small = cv2.resize(image_rgb,fx=scale,fy=scale)
-        face_encoding = face_recognition.face_encodings(image_bgr)
+        face_encoding = face_recognition.face_encodings(small)
+        face_location = face_recognition.face_locations(small)
+        face_distance = face_recognition.face_distance(small)
+        return {
+            "encodings":face_encoding,
+            "locations":face_location,
+            "distances":face_distance
+        }
+    
+    def switch_camera(self,camera):
+        self.camera.release()
+        self.camera = cv2.VideoCapture(camera)
+
+    def destroy(self):
+        self.camera.release()
+        cv2.destroyAllWindows()
