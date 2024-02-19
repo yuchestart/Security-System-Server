d28c5ef67bc4676930dd6bed342f70a0f51c1301
What-ever-this-is <yuchestart@gmail.com>
2024-02-19 10:20:28 -0600

    Optimized(hopefully) face recognition by reordering steps

--- src/facerec.py
+++ src/facerec.py
@@ -1,5 +1,6 @@
 import cv2
 import face_recognition
+import numpy as np
 from typing import *
 class Face():
     name:str = None
@@ -21,13 +22,13 @@ class FaceRecognition():
     def __init__(self):
         pass
         #self.camera = cv2.VideoCapture(camera)
-    def detect_faces(self,image_bgr,scale=0.25):
+    def detect_faces(self,image_bgr,scale=0.25) -> List[Face]:
 
         image_rgb = cv2.cvtColor(image_bgr,cv2.COLOR_BGR2RGB)
-        small = cv2.resize(image_rgb,fx=scale,fy=scale)
-
-        face_encodings = face_recognition.face_encodings(small)
+        small = cv2.resize(image_rgb,(0,0),fx=scale,fy=scale)
         face_locations = face_recognition.face_locations(small)
+        face_encodings = face_recognition.face_encodings(small,face_locations)
+        
 
         #There should be the same number of face encodings and locations
         faces = []
@@ -41,27 +42,27 @@ class FaceRecognition():
 
         return faces
     
-    def label_faces(self,encodings: List[Face]):
-        labels: List[Tuple[str,Face]] = []
+    def label_faces(self,faces: List[Face],threshold = 0.6) -> List[str]:
+        labels: List[str] = []
         encodings = []
+        for face in faces:
+            encodings.append(face.encoding)
+        face_categories = ["hostile","neutral","friendly"]
         for encoding in encodings:
             #First check hostile
-            face_categories = ["hostile","neutral","friendly"]
-            face_distances:Dict[str,float] = {"hostile":-1,"neutral":-1,"friendly":-1}
-            face_data:Dict[str,Face] = {"hostile":None,"neutral":None,"friendly":None}
+            face_distances:Dict[str,List[float]] = {"hostile":None,"neutral":None,"friendly":None}
+            face_matches:Dict[str,str] = {"hostile":"Stranger","neutral":"Stranger","friendly":"Stranger"}
             for category in face_categories:
-                distances = face_recognition.face_distance(list(map(lambda x: x.encoding,self.known_faces[category])),encoding)
-                for i,distance in enumerate(distances):
-                    if face_distances[category] == -1 or distance < face_distances[category]:
-                        face_distances[category] = distance
-                        face_data[category] = self.known_faces[category][i]
-            labels.append((
-                min(face_distances),
-                face_data[min(face_distances)]
-            ))
+                face_distances[category] = face_recognition.face_distance(
+                    list(map(lambda x: x.encoding,self.known_faces[category])),
+                    encoding
+                )
+                min_distance = np.argmin(face_distances[category])
+                if face_distances[min_distance] < threshold:
+                    face_matches[category] = self.known_faces[category][min_distance].name
+            labels.append(face_matches[min(face_distances,key=lambda x: face_distances[x])])
         return labels
-            
-                    
+
     def switch_camera(self,camera):
         if self.camera != None:
             self.camera.release()
@@ -70,8 +71,6 @@ class FaceRecognition():
     def destroy(self):
         self.camera.release()
         cv2.destroyAllWindows()
-
-
 
     def capture(self):
         return self.camera.read()
